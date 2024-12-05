1d72e142bc52615e6603fc6f2b536bea2db54991
Joshua A. Anderson <joaander@umich.edu>
2024-12-05 11:07:58 -0500

    Fix NEC integrators.

--- hoomd/hpmc/pytest/test_nec.py
+++ hoomd/hpmc/pytest/test_nec.py
@@ -3,11 +3,11 @@
 
 import hoomd
 
+
 def test_nec(simulation_factory, lattice_snapshot_factory):
     snap = lattice_snapshot_factory(particle_types=["A"])
     simulation = simulation_factory(snap)
     sphere = hoomd.hpmc.nec.integrate.Sphere()
-    sphere.shape['A'] = {'diameter': 1.0}
+    sphere.shape["A"] = {"diameter": 1.0}
     simulation.operations.integrator = sphere
     simulation.run(10)
-
