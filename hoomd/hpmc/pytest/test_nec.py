2b0b1d63a9a5fa92283c52ad0bc6ef2cd6c88d46
Joshua A. Anderson <joaander@umich.edu>
2024-12-05 10:21:21 -0500

    Fix and test Python initialization of NEC.

--- hoomd/hpmc/pytest/test_nec.py
+++ hoomd/hpmc/pytest/test_nec.py
@@ -6,6 +6,8 @@ import hoomd
 def test_nec(simulation_factory, lattice_snapshot_factory):
     snap = lattice_snapshot_factory(particle_types=["A"])
     simulation = simulation_factory(snap)
-    simulation.operations.integrator = hoomd.hpmc.nec.integrate.Sphere()
+    sphere = hoomd.hpmc.nec.integrate.Sphere()
+    sphere.shape['A'] = {'diameter': 1.0}
+    simulation.operations.integrator = sphere
     simulation.run(10)
 
