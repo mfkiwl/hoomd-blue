661d39e1b4f2be2144d96bc67b410ecf25631a14
Joshua A. Anderson <joaander@umich.edu>
2024-12-05 11:34:02 -0500

    Execute NEC test in serial.

--- hoomd/hpmc/pytest/test_nec.py
+++ hoomd/hpmc/pytest/test_nec.py
@@ -2,8 +2,10 @@
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 import hoomd
+import pytest
 
 
+@pytest.mark.serial
 def test_nec(simulation_factory, lattice_snapshot_factory):
     snap = lattice_snapshot_factory(particle_types=["A"])
     simulation = simulation_factory(snap)
