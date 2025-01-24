8c9e0647a0944b371961eb78c675d58900b53d77
martin <martin.girard@mpip-mainz.mpg.de>
2025-01-24 20:41:45 +0100

    fix test function names

--- hoomd/test/test_math.cc
+++ hoomd/test/test_math.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2024 The Regents of the University of Michigan.
+// Copyright (c) 2009-2025 The Regents of the University of Michigan.
 // Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 #include "hoomd/HOOMDMath.h"
@@ -10,8 +10,8 @@ using namespace hoomd;
 HOOMD_UP_MAIN()
 
 template<class T>
-void test_exact(){
-    // powers of two are exact
+void test_int_pow_exact(){
+    // test powf(T, int) functions for powers of 2, which should be exact
     T base = 2.0;
     std::vector<std::pair<int, T>> expected{
         {-3, T(1.0 / 8.0)},
@@ -28,7 +28,8 @@ void test_exact(){
     }
 
 template<class T>
-void test_close(){
+void test_int_pow_close(){
+    // test powf(T, int) functions and compare them against std::powf
     std::vector<T> bases{T(-3.234), T(-0.343), T(0.6545), T(2.5844)};
     for(int exponent = -5; exponent < 5; exponent++){
         for(auto&& base : bases)
@@ -37,8 +38,8 @@ void test_close(){
     }
 
 UP_TEST(integer_power){
-        test_exact<double>();
-        test_exact<float>();
-        test_close<double>();
-        test_close<float>();
+    test_int_pow_exact<double>();
+    test_int_pow_exact<float>();
+    test_int_pow_close<double>();
+    test_int_pow_close<float>();
     }
